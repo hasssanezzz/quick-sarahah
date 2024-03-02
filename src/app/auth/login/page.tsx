21098f120b89e74787f2a66fc04f343ec6a8cab1
Hassan Ezz <dhassanezz98@gmail.com>
2024-03-02 14:06:03 +0200

    update: edited auth page styles

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/AuthForms/LoginForm'
 
 function LoginPage() {
   return (
-    <main className="container mx-auto px-5 flex items-center justify-center min-h-screen flex-col gap-10">
+    <main className="container mx-auto px-5 flex items-center justify-center pt-10 flex-col gap-10">
       <h3 className="text-4xl sm:text-5xl font-semibold text-center">Login</h3>
       <LoginForm />
     </main>
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -2,7 +2,7 @@ import SignupForm from '@/components/AuthForms/SignupForm'
 
 function RegisterPage() {
   return (
-    <main className="container mx-auto px-5 flex items-center justify-center min-h-screen flex-col gap-10">
+    <main className="container mx-auto px-5 flex items-center justify-center pt-10 flex-col gap-10">
       <h3 className="text-4xl sm:text-5xl font-semibold text-center">Signup</h3>
       <SignupForm />
     </main>
